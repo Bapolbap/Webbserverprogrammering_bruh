9ec45dd1aa4c2055f2e34a5490b3893b7e6f239a
Fru1ty <pablo.jofre34@gmail.com>
2020-09-10 23:01:59 +0200

    uppgift 2 klar
    den med post request

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express()
 const port = 3000
 const clientDir = __dirname + "\\MyFirstServer\\client\\"
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.sendFile(clientDir + "index.html")
 })
@@ -13,6 +16,12 @@ app.get('/style', (req, res) => {
 
 app.get('/image', (req, res) => {
     res.sendFile(`${clientDir}acension.jpg`)
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body.name)
+    console.log(req.body.email)
+    res.redirect('/')
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
